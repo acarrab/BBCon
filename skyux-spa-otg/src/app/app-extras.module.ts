b5d5bce77e83658b5511e65a33cefbc1e905ee8a
Janne Ave <Janne.Ave@blackbaud.me>
2017-07-27 15:43:54 -0400

    Initial commit of data dashboard

--- skyux-spa-otg/src/app/app-extras.module.ts
+++ skyux-spa-otg/src/app/app-extras.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 
+import { Graph1Component } from './volunteering/graph-tiles/graph1/graph1.component';
+import { Graph2Component } from './volunteering/graph-tiles/graph2/graph2.component';
+
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
   providers: [],
-  entryComponents: []
+  entryComponents: [
+    Graph1Component,
+    Graph2Component
+  ],
+  declarations: [
+  ]
 })
 export class AppExtrasModule { }
---
+++ skyux-spa-otg/src/app/volunteering/graph-tiles/graph1/graph1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line
+  selector: 'div.graph1',
+  templateUrl: './graph1.component.html'
+})
+export class Graph1Component {
+
+}
