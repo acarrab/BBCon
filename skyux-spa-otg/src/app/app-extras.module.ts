b8da4eb0407c1e44268086161990331670f987bc
Janne Ave <Janne.Ave@blackbaud.me>
2017-07-27 15:48:26 -0400

    Declared d3 throughout whole application

--- skyux-spa-otg/src/app/app-extras.module.ts
+++ skyux-spa-otg/src/app/app-extras.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { D3Service } from 'd3-ng2-service';
+
 import { Graph1Component } from './volunteering/graph-tiles/graph1/graph1.component';
 import { Graph2Component } from './volunteering/graph-tiles/graph2/graph2.component';
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
-  providers: [],
+  providers: [D3Service],
   entryComponents: [
     Graph1Component,
     Graph2Component
