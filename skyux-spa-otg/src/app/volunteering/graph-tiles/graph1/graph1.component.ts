8bb33535f64a4e7341de6c3b4c695baa063253d7
Janne Ave <Janne.Ave@blackbaud.me>
2017-07-27 19:58:32 -0400

    Adding mocked data in data/volunteer.ts

--- skyux-spa-otg/src/app/volunteering/graph-tiles/graph1/graph1.component.ts
+++ skyux-spa-otg/src/app/volunteering/graph-tiles/graph1/graph1.component.ts
@@ -16,11 +16,56 @@ export class Graph1Component implements OnInit {
     this.parentNativeElement = element.nativeElement;
   }
 
+  // tslint:disable-next-line
   ngOnInit() {
-    let d3 = this.d3; // <-- for convenience use a block scope variable
-    let d3ParentElement: Selection<any, any, any, any>;
-    if (this.parentNativeElement !== undefined) {
-      d3ParentElement = d3.select(this.parentNativeElement); // <-- use the D3 select method 
-    }
+      console.log('Hello');
+    let d3 = this.d3;
+    let svg = d3.select('#graph1')
+        .attr('width', 300) // set its dimentions
+        .attr('height', 150);
+
+    let dataset = [];
+
+    let fileReader = new FileReader();
+    let text = '';
+    let file: File = new File([''], 'data.csv', {type: 'text/plain'});
+
+    fileReader.onload = function(e) {
+        text = fileReader.result;
+        console.log(text);
+    };
+
+    fileReader.readAsText(file);
+
+    console.log(text);
+/*
+    let numbers = [ 5, 4, 10, 1 ];
+    let data = [
+      { date: '2014-01-01', amount: 10 },
+      { date: '2014-02-01', amount: 20 },
+      { date: '2014-03-01', amount: 40 },
+      { date: '2014-04-01', amount: 80 }
+    ];
+
+    d3.min(numbers);
+
+    let x = d3.scaleTime()
+        .domain([
+        new Date(Date.parse('2014-01-01')),
+        new Date(Date.parse('2014-04-01'))
+        ])
+        .range([0, 300]);
+
+    x(new Date(Date.parse('2014-02-01')));
+    // 103.3811949976841
+
+    // x is the d3.scaleTime()
+    let xAxis = d3.axisBottom(x)
+    .ticks(4); // specify the number of ticks
+
+    svg.append('g')            // create a <g> element
+    .attr('class', 'x axis') // specify classes
+    .call(xAxis);            // let the axis do its thing
+*/
   }
 }
---
+++ skyux-spa-otg/src/data/volunteerobjectcreator.py
@@ -0,0 +1,104 @@
+import csv
+
+# Index of headers in volunteer tsv file
+EMAIL = 0
+EVENT_ID = 1
+# EVENT_CONNECTS = 2
+# CONNECTS = 3
+FIRST_NAME = 4
+LAST_NAME = 5
+NP = 6
+AVALANCHE_HOURS = 7
+IND_HOURS = 8
+TOTAL_CONNECTS = 9
+
+# Index of headers in volunteer tsv file
+EVENT_ID = 0
+EVENT_NAME = 1
+EVENT_DATE = 2
+
+with open('volunteers.tsv', 'r') as volunteers_file, open('events.tsv', 'r') as events_file, open('volunteers.ts', 'w') as output_ts_file:
+    output_ts_file.write('import { NonprofitEvent } from \'../contracts/NonprofitEvent.interface\';\n')
+    output_ts_file.write('import { Volunteer } from \'../contracts/Volunteer.interface\';\n')
+    output_ts_file.write('import { ConnectionNode } from \'../contracts/ConnectionNode.interface\';\n')
+    output_ts_file.write('\n')
+
+    # Volunteers By Non-Profit 
+    output_ts_file.write('let volunteerByNonprofitMocks: Array<Volunteer> = [\n')
+    volunteers_file = csv.reader(volunteers_file, delimiter='\t')
+
+    next(volunteers_file)   # skip headers
+
+    for row in volunteers_file:     # row is an array
+        output_ts_file.write('  {' + '\n' + 
+                             '    id: \'\',' + '\n' + 
+                             '    email: \'' + row[EMAIL] + '\',' + '\n' + 
+                             '    firstName: \'' + row[FIRST_NAME] + '\',' + '\n' + 
+                             '    lastName: \'' + row[LAST_NAME] + '\',' + '\n' + 
+                             '    nonprofitName: \'' + row[NP] + '\',' + '\n' + 
+                             '    eventId: \'' + row[EVENT_ID] + '\',' + '\n' + 
+                             '    hours: ' + row[IND_HOURS] + ',' + '\n' + 
+                             '    connections: ' + row[TOTAL_CONNECTS] + ',' + '\n' + 
+                             '    avalancheHours: ' + row[AVALANCHE_HOURS]  + '\n' + 
+                             '  },\n')
+        # print (row)
+    
+    output_ts_file.write('];\n')
+    # Volunteers By Non-Profit 
+
+    output_ts_file.write('\n')
+
+    # Events
+    output_ts_file.write('let nonprofitEventMocks: Array<NonprofitEvent> = [\n')
+    events_file = csv.reader(events_file, delimiter='\t')
+
+    next(events_file)       # skip headers
+
+    for row in events_file:
+        output_ts_file.write('  {' + '\n' + 
+                             '    id: \'' + row[EVENT_ID] + '\',' + '\n' + 
+                             '    name: \'' + row[EVENT_NAME] + '\',' + '\n' + 
+                             '    dateTime: \'' + row[EVENT_DATE] + '\'' + '\n' + 
+                             '  },\n')
+
+    output_ts_file.write('];\n')
+    # Events
+
+    output_ts_file.write('\n')
+
+    # Volunteers By Event
+    output_ts_file.write('''let volunteerByEventMocks: Array<Volunteer> = [{ 
+    id: '',
+
+    email: '',
+    firstName: '',
+    lastName: '',
+
+    nonprofitName: '',
+    eventId: '',
+
+    hours: 0,
+    connections: 0,
+    avalancheHours: 0
+    }];''')
+    # Volunteers By Event
+
+    output_ts_file.write('\n')
+
+    # Connection Tree Mock
+    output_ts_file.write('''let connectionTreeMock: ConnectionNode = {
+    id: '',
+    children: new Array<ConnectionNode>()
+    };''')
+    # Connection Tree Mock
+
+    output_ts_file.write('\n')
+
+    # Export Calls
+    output_ts_file.write('export let volunteerByNonprofitData = volunteerByNonprofitMocks;\n')
+    output_ts_file.write('export let nonprofitEventData = nonprofitEventMocks;\n')
+    output_ts_file.write('export let volunteerByEventData = volunteerByEventMocks;\n')
+    output_ts_file.write('export let connectionTreeData = connectionTreeMock;\n')
+    # Export Calls
+
+    output_ts_file.write('\n')
