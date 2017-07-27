61d7b8850222e9c6446acf71eab0a4a52943f62f
acarrab <acarrabba6@gmail.com>
2017-07-27 17:15:33 -0400

    updating contracts

--- vol-intel-service/Contracts.ts
+++ vol-intel-service/Contracts.ts
@@ -1,58 +1,34 @@
-interface EventId extends String {}
-interface VolunteerId extends String {}
-interface FormId extends String {}
 
-interface NPEvent {
-    id: String
-    name: String
-    date_time: String
-    nonProfitName: String
+export namespace Get {
+    export interface NPEvent {
+        id: String
+        name: String
+        date_time: String
+        nonProfitName: String
+    }
+    export interface Volunteer {
+        email: String
+        first_name: String
+        last_name: String
+
+        organization: String
+        eventId: String
+        id: String
+
+        hours: Number
+        connections: Number
+        avalanche_hours: Number
+    }
 }
 
-interface Volunteer {
-    id: String
-    first_name: String
-    last_name: String
-    email: String
-    hours: Number
-    connections: Number
-    avalanche_hours: Number
-    organization: String
-}
-
-
-interface EventEndpoints {
-    /*
-    [HttpGet]: /events
-    returns: A list of events
-    */
-    getEvents(): Array<NPEvent>
-
-    /*
-    [HttpGet]: /events/{eventId}
-    returns: An event
-    */
-    getEvent(id:EventId): NPEvent
-
-    /* 
-    [HttpGet]: 
-        /events/volunteers
-        /events/{eventId}/volunteers
-    returns: a list of volunteers for a particular event or for all
-    */
-    getVolunteers(eid?: EventId): Array<VolunteerId>
-
-    /*
-    []
-    */
-    getVolunteerInfo(vid: VolunteerId): Volunteer 
-
-
-    // [HttpGet]: /events/{eventId}/
-}   
-
-
-class VolIntelService {
-    
-    
+export namespace Post {
+    export interface Volunteer {
+        email: String
+        first_name: String
+        last_name: String
+    }
+    export interface NPEvent {
+        name: String
+        nonprofitName: String
+    }
 }
