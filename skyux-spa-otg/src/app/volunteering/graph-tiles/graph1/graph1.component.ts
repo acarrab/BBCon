12993449b8afbee3bfd6b1fd71dcd6cfb918e5af
Janne Ave <Janne.Ave@blackbaud.me>
2017-07-27 20:52:29 -0400

    Added new code to get data from service

--- skyux-spa-otg/src/app/volunteering/graph-tiles/graph1/graph1.component.ts
+++ skyux-spa-otg/src/app/volunteering/graph-tiles/graph1/graph1.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { D3Service, D3, Selection } from 'd3-ng2-service';
+import { VolIntelService } from '../../../../mocks/volIntel.service.mock';
 
 @Component({
   // tslint:disable-next-line
   selector: 'div.graph1',
   templateUrl: './graph1.component.html',
-  styleUrls: []
+  styleUrls: [],
+  providers: [ VolIntelService ]
 })
 export class Graph1Component implements OnInit {
   private d3: D3; // <-- Define the private member which will hold the d3 reference
   private parentNativeElement: any;
+  private intelService: VolIntelService;
+  private data: any;
 
-  constructor(element: ElementRef, d3Service: D3Service) {
+  constructor(element: ElementRef, d3Service: D3Service, intelService: VolIntelService) {
     this.d3 = d3Service.getD3(); // <-- obtain the d3 object from the D3 Service
     this.parentNativeElement = element.nativeElement;
+    this.intelService = intelService;
+    this.data = intelService.getVolunteersByNonprofit('');  // Don't need the non profit string name
   }
 
   // tslint:disable-next-line
