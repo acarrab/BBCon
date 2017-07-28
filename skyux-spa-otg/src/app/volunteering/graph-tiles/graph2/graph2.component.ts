d31cda461f9bcc02404f77fc20a057dfa08bf49a
Alexis Coulter <Alexis.Coulter@blackbaud.me>
2017-07-27 20:49:33 -0400

    LOOK AT THIS GRAPH

--- skyux-spa-otg/src/app/volunteering/graph-tiles/graph2/graph2.component.ts
+++ skyux-spa-otg/src/app/volunteering/graph-tiles/graph2/graph2.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { D3Service, D3 } from 'd3-ng2-service';
+
+import * as d3 from 'd3-selection';
+import * as d3Scale from "d3-scale";
+import * as d3Shape from "d3-shape";
+import * as d3Array from "d3-array";
+import * as d3Axis from "d3-axis";
+
+import { Stocks } from './data';
 
 @Component({
-  // tslint:disable-next-line
   selector: 'div.graph2',
-  templateUrl: './graph2.component.html'
+  templateUrl: './graph2.component.html',
+  styleUrls: []
 })
-export class Graph2Component {
+export class Graph2Component implements OnInit {
+  private d3: D3; // <-- Define the private member which will hold the d3 reference
+  private parentNativeElement: any;
 
+  constructor(element: ElementRef, d3Service: D3Service) {
+    this.d3 = d3Service.getD3(); // <-- obtain the d3 object from the D3 Service
+    this.parentNativeElement = element.nativeElement;
+    this.width = 900 - this.margin.left - this.margin.right ;
+    this.height = 500 - this.margin.top - this.margin.bottom;
+  }
+
+
+  private margin = {top: 20, right: 20, bottom: 30, left: 50};
+  private width: number;
+  private height: number;
+  private x: any;
+  private y: any;
+  private svg: any;
+  private line: d3Shape.Line<[number, number]>;
+
+  ngOnInit() {
+    this.initSvg()
+    this.initAxis();
+    this.drawAxis();
+    this.drawLine();
+  }
+
+  private initSvg() {
+    this.svg = d3.select("#graph2")
+                 .append("g")
+                 .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");;
+  }
+
+  private initAxis() {
+    this.x = d3Scale.scaleTime().range([0, this.width]);
+    this.y = d3Scale.scaleLinear().range([this.height, 0]);
+    this.x.domain(d3Array.extent(Stocks, (d) => d.date ));
+    this.y.domain(d3Array.extent(Stocks, (d) => d.value ));
+  }
+
+  private drawAxis() {
+
+    this.svg.append("g")
+          .attr("class", "axis axis--x")
+          .attr("transform", "translate(0," + this.height + ")")
+          .call(d3Axis.axisBottom(this.x));
+
+    this.svg.append("g")
+          .attr("class", "axis axis--y")
+          .call(d3Axis.axisLeft(this.y))
+          .append("text")
+          .attr("class", "axis-title")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Price ($)");
+  }
+
+  private drawLine() {
+    this.line = d3Shape.line()
+                       .x( (d: any) => this.x(d.date) )
+                       .y( (d: any) => this.y(d.value) );
+
+    this.svg.append("path")
+            .datum(Stocks)
+            .attr("class", "line")
+            .attr("d", this.line);
+  } 
 }
